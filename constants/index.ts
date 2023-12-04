fb7e112957efb6b4f4f95b74518105da8a00a675
Akshara Hegde <akshara@blowhorn.com>
2023-12-04 16:40:46 +0530

    feat - updated seo

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const META_TITLE = "Create Stunning Banners for Your Nuxt Modules";
+export const META_TITLE = "Create official banner for your nuxt modules";
 export const META_DESCRIPTION = "Generate professional banners for your Nuxt modules in minutes. Customize your banners to enhance your module's branding and visibility.";
